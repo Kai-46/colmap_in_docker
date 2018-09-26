2ed404e61b50919bb32912adb2505c447f89b262
Kai Zhang <kz298@cornell.edu>
2018-09-26 01:54:23 -0400

    Update run_colmap.sh

--- run_colmap.sh
+++ run_colmap.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 
-# usage: docker_run.sh [options] <image_name> <command inside the container>
-# some of the popular options are
-	# to run container in background, use -d
-	# to run container interactively, use -ti
-	# to autoremove the container after it exits, use --rm
-
-# example: docker_run.sh -ti --rm ubuntu:18.04 bash
-#		   docker_run.sh -ti ubuntu:18.04 bash
-
 USER_ID=$(id -u)
-# CONTAINER_NAME=${USER}_$(date +%m%d_%H%m%S)
 
-# echo "starting container with uid: ${USER_ID}..."
-# echo "host volume '/phoenix' has been mounted..."
-# echo "this container has the same permission access to the mounted host volumes as ${USER} (uid: ${USER_ID})"
 docker run \
     -ti --rm \
 	-u ${USER_ID} \
@@ -22,6 +9,5 @@ docker run \
 	--hostname="colmap-in-docker" \
     --runtime=nvidia \
 	-v /phoenix:/phoenix \
-	-v /etc/localtime:/etc/localtime:ro \
 	kai46/colmap:latest \
     bash
